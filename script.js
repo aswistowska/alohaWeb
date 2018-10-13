f2bc5cc21c5f13029ebdb56ed6dc5052452dc890
Agata <agata@swistowski.org>
2018-10-13 23:06:05 +0100

    added: jQuery, smooth scrolling, carousel (flickity), email subscribing alerts; changed container width;

---
+++ script.js
@@ -0,0 +1,37 @@
+$(function () {
+    let emailElement = $('#email');
+
+    // Smooth scrolling header elements
+    $('header nav a')
+        .click(function (event) {
+            var target = $(this.hash);
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        });
+
+    $('#submit-button').click(function (event) {
+        event.preventDefault();
+        let email = emailElement.val();
+        if(validateEmail(email)){
+            alert('Thanks for subscribing!');
+        } else {
+            alert('Please enter a valid email address');
+            emailElement.focus();
+        }
+    });
+
+    $('.carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        wrapAround: true
+    });
+
+});
+
+function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
